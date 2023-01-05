d6f163c8046352c9b59310679566f7a602ec09a5
Kat-Devs <katlegophele95@gmail.com>
2023-01-05 10:34:52 -0800

    PEP fixes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-#Print all possible different combinations 
-#of 2 numbers (Base 10)
-
-for i in range(0,100):
+for i in range(0, 100):
     if i % 10 > i / 10:
         if i != 89:
             print("{:02d}".format(i), end=', ')
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    if number >= 0:
+        last_num = number % 10
+    else:
+        last_num = number % -10
+        last_num *= -1
+
+    print("{:d}".format(last_num),end='')
+    return (last_num)
