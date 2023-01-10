c8bb3d643257cb850e7a8baf809068632dd8532d
Kat-Devs <113664624+Kat-Devs@users.noreply.github.com>
2023-01-10 03:54:58 +0200

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-
 def replace_in_list(my_list, idx, element):
-    if idx > (len(my_list) - 1) or idx < 0:
-        return my_list
-    else:
-        my_list[idx] = element
-        return 
+    if idx < 0:
+        return (my_list)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (my_list)
+
+    my_list[idx] = element
+
+    return (my_list)
