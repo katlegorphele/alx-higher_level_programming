d8f3bd8702496102f364e597f53ac1e7fd8284eb
Kat-Devs <katlegophele95@gmail.com>
2023-03-16 03:05:16 +0200

    minor updates

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa.cities' (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KLEY(state_id) REFERENCES hbtn_0d_usa.states(id));
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
 
