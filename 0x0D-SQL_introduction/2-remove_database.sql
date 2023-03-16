5e36e130ef648af073086088182993e765090980
Kat-Devs <katlegophele95@gmail.com>
2023-03-16 03:26:01 +0200

    task 0x0D files with corrections

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,2 @@
--- Script that deletes the database hbtn_0c_0 in MySQL server.
-
+-- Script that deletes the database hbtn_0c_0 in your MySQL server
 DROP DATABASE IF EXISTS hbtn_0c_0;
-
--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,2 @@
--- script that lists all the tables of a database in your MySQL server.
--- The database name will be passed as argument of mysql command
-
+-- Script that lists all the tables of a database in your MySQL server
 SHOW TABLES;
