3ce4da17ad70c8aeafe716f2b376c44980fa5628
Kat-Devs <katlegophele95@gmail.com>
2023-04-12 05:59:53 +0200

    advanced task fixes

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,15 +1,7 @@
 #!/usr/bin/node
 
 function executeXTimes(x, theFunction) {
-    for (let i = 0; i < x; i++) {
-      theFunction();
-    }
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
 }
-  
-  // Example usage:
-  function printHello() {
-    console.log('Hello!');
-}
-  
-executeXTimes(5, printHello); // Output: Hello! Hello! Hello! Hello! Hello!
-  
--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -4,10 +4,3 @@ function incrementAndCall(number, theFunction) {
   number++;
   theFunction(number);
 }
-
-// Example usage:
-function printNumber(number) {
-  console.log(number);
-}
-
-incrementAndCall(5, printNumber); // Output: 6
