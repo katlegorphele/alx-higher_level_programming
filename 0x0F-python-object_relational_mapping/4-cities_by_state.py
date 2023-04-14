1eb01200468cced6f3d17e706d1ae8ad14f4541c
Kat-Devs <katlegophele95@gmail.com>
2023-04-14 17:10:21 +0200

    Task 0 - 5 added

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,29 +1,29 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-#script that lists all cities from the database hbtn_0e_4_usa
+# script that lists all cities from the database hbtn_0e_4_usa
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
 
-    #Get command line args
+    # Get command line args
     u_name = sys.argv[1]
     sql_pass = sys.argv[2]
     sql_db = sys.argv[3]
 
-    #Connect to DB
+    # Connect to DB
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user = u_name,
+        user=u_name,
         passwd=sql_pass,
-        db = sql_db
+        db=sql_db
     )
 
     # Create cursor object
     cursor = db.cursor()
 
-    #construct SQL Query
+    # construct SQL Query
     sql_query = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
 
     # Execute SQL Query
@@ -36,4 +36,3 @@ if __name__ == '__main__':
 
     # Close db connection
     db.close()
-    
