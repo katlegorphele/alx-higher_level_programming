f9b4a070bbaf89d1c9119126d37e99a24397756f
Kat-Devs <katlegophele95@gmail.com>
2023-04-28 20:43:53 +0200

    0x10,code update.

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
+curl -sLXH PUT "Origin: X-School-User-Id" -d "user_id=98" 0.0.0.0:5000/catch_me
